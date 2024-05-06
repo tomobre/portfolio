3d07645778e0cf9cd29df62123bb65243efe63d1
tobregon-minery <tobregon@mineryreport.com>
2024-05-06 20:55:50 +0200

    finished

--- src/components/TransitionProviderClient.jsx
+++ src/components/TransitionProviderClient.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AnimatePresence } from "framer-motion";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
